620719a7de5e0653b2a967458749a7be5c8646b8
Juan <0xjuaan@gmail.com>
2025-08-24 18:11:46 +0800

    base layout of sidebar

--- vocs.config.ts
+++ vocs.config.ts
@@ -11,20 +11,27 @@ export default defineConfig({
   },
   sidebar: [
     {
-      text: 'Getting Started',
-      link: '/getting-started',
+      text: 'Introduction',
+      items: [
+        { text: 'Getting Started', link: '/getting-started' },
+      ],
     },
     {
-      text: 'Components',
+      text: 'Development',
       items: [
         { text: 'CoreWriterLib', link: '/corewriter' },
         { text: 'PrecompileLib', link: '/precompiles' },
         { text: 'TokenRegistry', link: '/token-registry' },
       ],
     },
-    { text: 'Testing Framework', link: '/testing-framework' },
+    {
+      text: 'Testing Framework',
+      items: [
+        { text: 'Quick start', link: '/testing-framework' },
+        { text: 'Cheatcodes', link: '/cheatcodes' },
+      ],
+    },
     { text: 'Examples', link: '/example' },
-    { text: 'Security', link: '/security' },
     { text: 'Contributing', link: '/contributing' },
   ],
 })
